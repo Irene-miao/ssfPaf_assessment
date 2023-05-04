e10adc0d3db4d8fe7bc7d024e03b72c1b8c08237
Irene-miao <71961907+Irene-miao@users.noreply.github.com>
2023-05-04 09:06:51 +0800

    First commit

---
+++ src/main/java/ibf2022/batch3/paf/server/controllers/RestaurantController.java
@@ -0,0 +1,16 @@
+package ibf2022.batch3.paf.server.controllers;
+
+public class RestaurantController {
+
+	// TODO: Task 2 - request handler
+	
+
+	// TODO: Task 3 - request handler
+
+
+	// TODO: Task 4 - request handler
+	
+
+	// TODO: Task 5 - request handler
+
+}
